e348b7663c1e2da92c4cdbd32c50cbd92a7fd65d
danieljayasutra <danieljayasutra@gmail.com>
2024-12-05 22:51:23 +0700

    choose level

--- src/views/ChooseLevel.vue
+++ src/views/ChooseLevel.vue
@@ -1,3 +1,50 @@
 <template>
-  <div>Hello</div>
+  <div class="w-full h-screen flex items-end px-[5%] lg:px-[10%]">
+    <div class="w-full h-screen flex flex-col justify-between gap-5 py-[20%] lg:py-[15%]">
+      <div class=" flex justify-center lg:justify-start">
+        <div class="w-[150px] lg:w-[200px]">
+           <img src="../assets/word-echo.svg" alt="" />
+        </div>
+      </div>
+      <div class="flex justify-end">
+        <div class="grid grid-cols-12 gap-8">
+          <div class=" col-span-12 lg:col-span-6 space-y-8">
+            <div class="text-cyan text-4xl font-bold text-center lg:text-right">Choose Level</div>
+            <div class="text-center flex flex-col items-center lg:items-end  space-y-6 font-bold text-xl">
+              <div class="text-orange flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                 <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                </svg>
+                <span>Entry Level</span>
+              </div>
+              <div class=" flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                 <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                </svg>
+                <span>Intermediet</span>
+              </div>
+              <div class=" flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                 <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                </svg>
+                <span>Hard</span>
+              </div>
+            </div>
+          </div>
+          <div class="lg:col-span-1 flex justify-center">
+            <div class="bg-white w-[5px] h-full"></div>
+          </div>
+          <div class="col-span-12 lg:col-span-5 space-y-5 text-center lg:text-left">
+            <div class=" space-y-3">
+              <div class="text-cyan text-xl font-bold">Entry Level</div>
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/> Atque, consequatur! Enim aliquam repudiandae explicabo.</div>
+            </div>
+            <div class="flex justify-center lg:justify-start">
+              <button @click="$router.push('/choose-level')" class="bg-orange px-10 lg:px-32 text-black rounded-xl py-4 text-2xl font-bold">Start</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="w-full h-screen flex items-end p-[10%]">
-    <div class="w-full">
-      <div class="w-[500px]">
-        <img src="../assets/word-echo.svg" alt="" />
+  <div class="w-full h-screen flex items-end px-[5%] py-[10%] lg:p-[10%]">
+    <div class="w-full space-y-20 lg:space-y-40">
+      <div class=" flex justify-center lg:justify-start">
+        <div class="w-[250px] lg:w-[500px]">
+           <img src="../assets/word-echo.svg" alt="" />
+        </div>
       </div>
       <div class="flex justify-end">
         <div class="grid grid-cols-12 gap-8">
-          <div class="col-span-6 space-y-8">
-            <div class="text-right space-y-8">
+          <div class=" col-span-12 lg:col-span-6 space-y-8">
+            <div class="text-center lg:text-right space-y-8">
               <div>
                 Made as an assignment <br />
                 for Smart Web Programming
@@ -17,14 +19,16 @@
                 Mercubuana University
               </div>
             </div>
-            <button class="bg-orange px-32 text-black rounded-xl py-4 text-2xl font-bold">Play</button>
+            <div class="flex justify-center">
+              <button @click="$router.push('/choose-level')" class="bg-orange px-10 lg:px-32 text-black rounded-xl py-4 text-2xl font-bold">Play</button>
+            </div>
           </div>
-          <div class="col-span-1 flex justify-center">
+          <div class="lg:col-span-1 flex justify-center">
             <div class="bg-white w-[5px] h-full"></div>
           </div>
-          <div class="col-span-5">
+          <div class="col-span-12 lg:col-span-5 space-y-5 text-center lg:text-left">
             <div class="text-cyan text-4xl font-bold">Creator</div>
-            <div>
+            <div class=" space-y-3">
               <div>Daniel Jayasutra</div>
               <div>Septian Pramana R</div>
               <div>Alya Hamida</div>
